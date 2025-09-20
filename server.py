fca63b87bc8659aa437b93bf241cd1264638682c
harisaravanan-kjhjk <harisaravanan033@gmail.com>
2025-09-20 17:27:59 +0530

    Your descriptive message

--- server.py
+++ server.py
@@ -20,5 +20,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
